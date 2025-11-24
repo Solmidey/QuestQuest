cb5eb076877ce4218fbc34a8368d92bfe1f39aef
Solmidey <122814155+Solmidey@users.noreply.github.com>
2025-11-24 03:34:58 +0100

    Add contract address and ABI imports

---
+++ src/lib/contract.ts
@@ -0,0 +1,6 @@
+import QuestQuestABI from "../abis/QuestQuest.json";
+import AchievementsABI from "../abis/AchievementsNFT.json";
+
+export const QUESTQUEST_ADDRESS = "YOUR_CONTRACT_ADDRESS";
+export const ABI_QUEST = QuestQuestABI;
+export const ABI_NFT = AchievementsABI;
