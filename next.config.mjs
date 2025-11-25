6bfa4a38677b471741511b40a07f372e9ec9df61
Solmidey <122814155+Solmidey@users.noreply.github.com>
2025-11-25 20:58:03 +0000

    Add manifest.json with all required images

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
